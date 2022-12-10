7149070771c83d739fe829070294ff500893b34f
Zaidabdullah <58549696+Zaidabdullah@users.noreply.github.com>
2022-12-10 13:38:55 +0000

    added

--- OOP applications/main.cc
+++ OOP applications/main.cc
@@ -10,7 +10,7 @@ int main()
    clsDate Date1(1,1,2000); clsDate Date2(2,2,2000) ;
     cout << clsInputValidate::isValidDate(clsDate(35,1,2000)) << endl;
     cout << clsInputValidate::isDateBetween(clsDate(4,1,2000), Date1, Date2)  << endl;
-    cout << clsInputValidate::isValidDate(clsDate(35,1,2000));
+    cout << clsInputValidate::IsNumberBetween(1,4, 1) << endl;
     // cout << clsInputValidate::isValidDate(clsDate(35,1,2000));
     // cout << clsInputValidate::isValidDate(clsDate(35,1,2000));
     // cout << clsInputValidate::isValidDate(clsDate(35,1,2000));
