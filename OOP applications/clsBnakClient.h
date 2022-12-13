cce230612be3ce7fd2705ce9812375350f20fc33
Zaidabdullah <zaid.engi@gmail.com>
2022-12-13 20:01:39 +0000

    add print method to clsBnakClient

--- OOP applications/clsBnakClient.h
+++ OOP applications/clsBnakClient.h
@@ -21,10 +21,46 @@ class clsBankClient : clsPerson
             _balance = balance;
       }
 
+      string Account_ID()
+      {
+        return _Account_ID;
+      }
+
+      string PinCode()
+      {
+        return _PinCode;
+      }
       
+      void setPinCode(string PinCode)
+      {
+         _PinCode = PinCode;
+      }
 
+      string balance()
+      {
+        return balance;
+      }
 
+      void balance(int balance)
+      {
+        _balance = balance;
+      }
 
+      void print()
+      {
+          cout << "first name "   << FirstName()  << endl;
+          cout << "last name "    << LastName()   << endl;
+          cout << "email "        << Email()      << endl;
+          cout << "phone number " << PhoneNumber()<< endl;
+          cout << "account ID "   << Account_ID() << endl;
+          cout << "pin code "     << PinCode()    << endl;
+          cout << "balance "      << balance()    << endl;
+      }
+
+    static find(string acount ID)
+    {
+        
+    }
 
 
 
